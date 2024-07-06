f8110326c17ba02a38a0bbcd7d649e7066df783e
JuanManuelF93 <freibergerjuan@gmail.com>
2024-07-06 16:46:03 -0300

    Codigo final

---
+++ module3-solution/app.js
@@ -0,0 +1,78 @@
+(function () {
+    'use-strict';
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems)
+        .constant('url', "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json");
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var list = this;
+        list.found = "";
+        list.searchTerm = "";
+        list.foundList = function () {
+            MenuSearchService.getMatchedMenuItems(list.searchTerm).then(function (result) {
+                list.found = result;
+            });
+        };
+
+        list.removeItem = function (parentIndex, itemIndex) {
+            console.log("list: ", list.found, "parentIndex: ", parentIndex, "itemIndex: ", itemIndex);
+            list.found[parentIndex]['menu_items'].splice(itemIndex, 1);
+        };
+
+    }
+    MenuSearchService.$inject = ['$http', 'url'];
+    function MenuSearchService($http, url) {
+        var service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({ url: url })
+                .then(function (result) {
+                    var foundItems = result.data;
+                    var filteredItems = {};
+                    if (searchTerm.trim() !== "") {
+                        // Iterar sobre cada clave en foundItems (A, B, C, ...)
+                        angular.forEach(foundItems, function (categoryValue, categoryKey) {
+                            console.log("categoryKey: ", categoryKey, "categoryValue: ", categoryValue);
+
+                            // Crear una copia de la estructura de la categoría actual
+                            var categoryCopy = angular.copy(categoryValue);
+
+                            // Filtrar los menu_items de la categoría actual
+                            categoryCopy.menu_items = categoryValue.menu_items.filter(function (item) {
+                                // Verificar si el description contiene searchTerm usando includes
+                                return item.description.toLowerCase().includes(searchTerm.toLowerCase());
+                            });
+
+                            // Agregar la categoría filtrada al objeto de elementos filtrados solo si tiene items después de filtrar
+                            if (categoryCopy.menu_items.length > 0) {
+                                // Usamos categoryKey como la clave en filteredItems
+                                filteredItems[categoryKey] = categoryCopy;
+                            }
+                        });
+                        console.log("filteredItems: ", filteredItems);
+                        foundItems = filteredItems;
+                        return foundItems;
+                    }
+                    return foundItems;
+                });
+        };
+    }
+
+    function FoundItems() {
+        var ddo = {
+            // template: '{{ found }}',
+            templateUrl: 'foundList.html',
+            scope: {
+                found: '<',
+                // myTitle: '@title',
+                // badRemove: '=',
+                onRemove: '&'
+            }
+        };
+        return ddo;
+    }
+
+})();
